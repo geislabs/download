8083de4e2d09984c728200a68cf27a758a91b3a8
mads hargreave <mads.hargreave@gmail.com>
2021-06-04 13:54:50 +0200

    released

--- packages/download-fetch/lib/fetchProvider.ts
+++
@@ -1,8 +0,0 @@
-import { FetchAdapter } from '@geislabs/fetch'
-// import { Json, JsonPath } from '@geislabs/json-type'
-
-// export const createFetch = (): FetchAdapter<'json', JsonPath> => ({
-//     name: 'json',
-//     encode: (value) => JSON.stringify(value.valueOf()),
-//     decode: (value) => Json(value),
-// })
